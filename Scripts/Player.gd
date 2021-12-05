21ba9d1228116dd0a3367d6e3bdce641e8941e9d
AlexAbt <alexaub45.aa@gmail.com>
2021-12-05 18:19:20 +0100

    Add Player.tscn
    Ajout du joueur avec animation des mouvements, roulades, attaques. Manque les hitbox et hurtbox.
    Ajout d'une caméra suivant le joueur dans la scène Prison.tscn

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,33 +1,83 @@
 extends KinematicBody2D
 
-var max_speed:float = 300
-var acceleration:float = 3600
-var friction:float = 3600
+# Variables accessibles dans l'onglet à droite en sélectionnant Player.tscn
+export var ACCELERATION = 500
+export var MAX_SPEED = 100
+export var ROLL_SPEED = 140
+export var FRICTION = 500
+
+# Enum pour machine état des animations du joueur
+enum {
+	MOVE,   # State 0
+	ROLL,   # State 1
+	ATTACK  # State 2
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
-var attacking:bool = false
+var roll_vector = Vector2.DOWN # Initialement Player regarde vers le bas
+#var stats = $PlayerStats # Singleton Autoload
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _ready():
-	pass
+	animationTree.active = true
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			roll_state(delta)
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
-	if OS.get_latin_keyboard_variant() == "AZERTY":
-		input_vector.x = int(Input.is_action_pressed("D")) - int(Input.is_action_pressed("Q"))
-		input_vector.y = int(Input.is_action_pressed("S")) - int(Input.is_action_pressed("Z"))
-	else:
-		input_vector.x = int(Input.is_action_pressed("D")) - int(Input.is_action_pressed("A"))
-		input_vector.y = int(Input.is_action_pressed("S")) - int(Input.is_action_pressed("W"))
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-	if input_vector != Vector2.ZERO:
-		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
+	
+	if input_vector != Vector2.ZERO: # When moving
+		roll_vector = input_vector # Effectue une roulade dans le même sens que la course
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	move()
+	
+	# Roulade/Esquive (non implémentée -> idée enlever temporairement hitbox)
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func move():
 	velocity = move_and_slide(velocity)
 
-func _unhandled_input(event):
-	if Input.is_action_just_pressed("left_click"):
-		attacking = true
+# Actions de l'utilisateur
+func roll_state(delta):
+	velocity = roll_vector * ROLL_SPEED # Attribue un vecteur au mvmt roulade
+	animationState.travel("Roll") # Passe de Run/Idle à Roll dans AnimationTree pour jouer l'animation
+	move() # Effectue le déplacement
 
-func _input(event):
-	if Input.is_action_just_released("left_click"):
-		attacking = false
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+
+# Fin des animations et changements d'états vers Idle/Run
+func roll_animation_finished():
+	velocity = velocity / 4
+	state = MOVE
+
+func attack_animation_finished():
+	state = MOVE
