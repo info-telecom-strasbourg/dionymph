497b7d4b3c692a16c61f606d65e43697225281d6
Apple0726 <bpm_apple@hotmail.com>
2022-01-29 14:08:54 +0100

    Push changes

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -30,3 +30,23 @@ func _process(delta):
 		cloud.position.x -= 1.0
 		if cloud.position.x < -200:
 			cloud.position.x = 1000
+
+
+func _on_ButtonNewG_mouse_entered():
+	var tween = Tween.new()
+	add_child(tween)
+	tween.interpolate_property($ButtonNewG.material, "shader_param/strength", null, 1.0, 0.5)
+	tween.start()
+	yield(tween, "tween_all_completed")
+	remove_child(tween)
+	tween.queue_free()
+
+
+func _on_ButtonNewG_mouse_exited():
+	var tween = Tween.new()
+	add_child(tween)
+	tween.interpolate_property($ButtonNewG.material, "shader_param/strength", null, 0.0, 0.2)
+	tween.start()
+	yield(tween, "tween_all_completed")
+	remove_child(tween)
+	tween.queue_free()
